def3f3adcda9f9d0d6f10b350aaa8ccedf7c470f
Mdshahed1234 <114864779+Mdshahed1234@users.noreply.github.com>
2024-03-01 15:54:51 +0530

    add one h1 tag

---
+++ frontend/chat-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Shahed from "./Components/Shahed";
+
+function App() {
+  return (
+    <div className="App">
+      <Shahed />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/chat-app/src/Components/Shahed.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const shahed = () => {
+  return (
+    <div>
+      <h1>hello shahed is here</h1>
+    </div>
+  );
+};
+
+export default shahed;
